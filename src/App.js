ea286e519b84d7889f4577487d30e36369e3f92a
josepehoyos <josepehoyos@gmail.com>
2022-07-05 00:00:35 -0500

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ export default function App() {
   return (
     <div>
     <nav>
-        <a href="#" className="nav-enlace desaparece">Inicio</a>
-        <a href="#" className="nav-enlace desaparece">Nosotros</a>
-        <a href="#" className="nav-enlace desaparece">Servicios</a>
-        <a href="#" className="nav-enlace desaparece">Contacto</a>
+        <a href="/" className="nav-enlace desaparece">Inicio</a>
+        <a href="/" className="nav-enlace desaparece">Nosotros</a>
+        <a href="/" className="nav-enlace desaparece">Servicios</a>
+        <a href="/" className="nav-enlace desaparece">Contacto</a>
     </nav>
     </div>
   );
